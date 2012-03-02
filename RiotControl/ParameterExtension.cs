603002a71f9e690a0a55a2e3dae336368c98d6d1
void <git.email.address@googlemail.com>
2012-03-02 14:33:21 +0100

    Removed the region enumeration code and worked out more summoner update code

---
+++ RiotControl/ParameterExtension.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Npgsql;
+using NpgsqlTypes;
+
+namespace RiotControl
+{
+	static class ParameterExtension
+	{
+		public static void Add(this NpgsqlParameterCollection parameters, string name, NpgsqlDbType type)
+		{
+			parameters.Add(new NpgsqlParameter(name, type));
+		}
+
+		public static void Set(this NpgsqlParameterCollection parameters, List<string>.Enumerator iterator, NpgsqlDbType type, object value)
+		{
+			parameters.Add(iterator.Current, type);
+			parameters[parameters.Count - 1].Value = value;
+			iterator.MoveNext();
+		}
+
+		public static void Set(this NpgsqlParameterCollection parameters, List<string>.Enumerator iterator, int value)
+		{
+			parameters.Set(iterator, NpgsqlDbType.Integer, value);
+		}
+
+		public static void Set(this NpgsqlParameterCollection parameters, List<string>.Enumerator iterator, string value)
+		{
+			parameters.Set(iterator, NpgsqlDbType.Text, value);
+		}
+	}
+}
--- RiotControl/RegionHandler.cs
+++ RiotControl/RegionHandler.cs
@@ -1,10 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 
-using LibOfLegends;
 using Npgsql;
 using NpgsqlTypes;
+
+using LibOfLegends;
+
+using com.riotgames.platform.statistics;
+using com.riotgames.platform.summoner;
 
 namespace RiotControl
 {
@@ -45,23 +50,109 @@ namespace RiotControl
 				WriteLine("There was an error connecting to the server");
 		}
 
-		void LookupSummoner(string summonerName)
+		void UpdateSummoner(int id, int accountId)
 		{
+		}
+
+		string GetGroupString(List<string> fields)
+		{
+			return String.Join(", ", fields);
+		}
+
+		string GetPlaceholderString(List<string> fields)
+		{
+			var mapped = from x in fields
+						 select ":" + x;
+			return GetGroupString(mapped.ToList());
+		}
+
+		string Zulufy(string input)
+		{
+			return string.Format("{0} at time zone 'UTC'", input);
+		}
+
+		void UpdateSummonerByName(string summonerName)
+		{
+			NpgsqlCommand nameLookup = new NpgsqlCommand("select id, account_id from summoner where summoner_name = :name", Database);
+			nameLookup.Parameters.Add(new NpgsqlParameter("name", NpgsqlDbType.Text));
+			NpgsqlDataReader reader = nameLookup.ExecuteReader();
+			if (reader.Read())
+			{
+				//The summoner already exists in the database
+				int id = (int)reader[0];
+				int accountId = (int)reader[1];
+				UpdateSummoner(id, accountId);
+			}
+			else
+			{
+				//We are dealing with a new summoner
+				PublicSummoner summoner = RPC.GetSummonerByName(summonerName);
+				if (summoner == null)
+				{
+					WriteLine("No such summoner: {0}", summonerName);
+					return;
+				}
+				List<string> coreFields = new List<string>()
+				{
+					"region",
+					"account_id",
+					"summoner_id",
+					"summoner_name",
+					"internal_name",
+					"summoner_level",
+					"profile_icon",
+				};
+
+				List<string> extendedFields = new List<string>()
+				{
+					"time_created",
+					"time_updated",
+				};
+
+				var field = coreFields.GetEnumerator();
+
+				string fieldsString = GetGroupString(coreFields.Concat(extendedFields).ToList());
+				string placeholderString = GetPlaceholderString(coreFields);
+				string currentTimestamp = Zulufy("current_timestamp");
+				string valuesString = string.Format("{0}, time_created = {1}, time_updated = {1}", placeholderString, currentTimestamp);
+
+				NpgsqlCommand newSummoner = new NpgsqlCommand(string.Format("insert into summoner ({0}) values ({1})", fieldsString, valuesString), Database);
+
+				newSummoner.Parameters.Set(field, NpgsqlDbType.Varchar, RegionProfile.RegionEnum);
+				newSummoner.Parameters.Set(field, summoner.acctId);
+				newSummoner.Parameters.Set(field, summoner.summonerId);
+				newSummoner.Parameters.Set(field, summonerName);
+				newSummoner.Parameters.Set(field, summoner.internalName);
+				newSummoner.Parameters.Set(field, summoner.summonerLevel);
+				newSummoner.Parameters.Set(field, summoner.profileIconId);
+
+				newSummoner.ExecuteNonQuery();
+
+				int id = InsertId("summoner");
+				UpdateSummoner(id, summoner.acctId);
+			}
+		}
+
+		int InsertId(string tableName)
+		{
+			NpgsqlCommand currentValue = new NpgsqlCommand(string.Format("select currval('{0}_id_seq')", tableName), Database);
+			int id = (int)currentValue.ExecuteScalar();
+			return id;
 		}
 
 		void Run()
 		{
-			NpgsqlCommand selectCommand = new NpgsqlCommand("select id, summoner_name from lookup_job where region = cast(:region as region_type) order by priority desc, time_added limit 1", Database);
-			selectCommand.Parameters.Add(new NpgsqlParameter("region", NpgsqlDbType.Varchar));
-			selectCommand.Parameters[0].Value = RegionProfile.RegionEnum;
+			NpgsqlCommand getJob = new NpgsqlCommand("select id, summoner_name from lookup_job where region = cast(:region as region_type) order by priority desc, time_added limit 1", Database);
+			getJob.Parameters.Add(new NpgsqlParameter("region", NpgsqlDbType.Varchar));
+			getJob.Parameters[0].Value = RegionProfile.RegionEnum;
 
-			NpgsqlCommand deleteCommand = new NpgsqlCommand("delete from lookup_job where id = :id", Database);
-			deleteCommand.Parameters.Add(new NpgsqlParameter(":id", NpgsqlDbType.Integer));
+			NpgsqlCommand deleteJob = new NpgsqlCommand("delete from lookup_job where id = :id", Database);
+			deleteJob.Parameters.Add(new NpgsqlParameter("id", NpgsqlDbType.Integer));
 
 			while (true)
 			{
 				NpgsqlTransaction lookupTransaction = Database.BeginTransaction();
-				NpgsqlDataReader reader = selectCommand.ExecuteReader();
+				NpgsqlDataReader reader = getJob.ExecuteReader();
 				bool success = reader.Read();
 
 				int id;
@@ -80,7 +171,7 @@ namespace RiotControl
 				lookupTransaction.Commit();
 
 				if (success)
-					LookupSummoner(summonerName);
+					UpdateSummonerByName(summonerName);
 				else
 				{
 					WriteLine("No jobs available");
