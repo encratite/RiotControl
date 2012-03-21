9d21bf78f905fc7edc030b2523f2cee67de5c07b
void <git.email.address@googlemail.com>
2012-03-21 12:42:04 +0100

    Did some refactoring and changed the Worker interface to provide more information about RPC results

--- RiotControl/Worker/UpdateSummonerByName.cs
+++ RiotControl/Worker/UpdateSummonerByName.cs
@@ -10,57 +10,67 @@ namespace RiotControl
 	partial class Worker
 	{
 		//Returns true if the summoner was updated successfully, false otherwise
-		public bool UpdateSummonerByName(string summonerName, ref int accountId)
+		public WorkerResult UpdateSummonerByName(string summonerName, ref int accountId)
 		{
-			//Attempt to retrieve an existing account ID to work with in order to avoid looking up the account ID again
-			//Perform lower case comparison to account for misspelled versions of the name
-			//LoL internally merges these to a mangled "internal name" for lookups anyways
-			using (var nameLookup = Command("select id, account_id, summoner_name from summoner where region = :region and lower(summoner_name) = lower(:name)"))
+			if (!Connected)
+				return WorkerResult.NotConnected;
+
+			try
 			{
-				nameLookup.Set("region", Profile.Identifier);
-				nameLookup.Set("name", summonerName);
-				using (var nameReader = nameLookup.ExecuteReader())
+				//Attempt to retrieve an existing account ID to work with in order to avoid looking up the account ID again
+				//Perform lower case comparison to account for misspelled versions of the name
+				//LoL internally merges these to a mangled "internal name" for lookups anyways
+				using (var nameLookup = Command("select id, account_id, summoner_name from summoner where region = :region and lower(summoner_name) = lower(:name)"))
 				{
-					if (nameReader.Read())
+					nameLookup.Set("region", Profile.Identifier);
+					nameLookup.Set("name", summonerName);
+					using (var nameReader = nameLookup.ExecuteReader())
 					{
-						//The summoner already exists in the database
-						int id = nameReader.Integer();
-						accountId = nameReader.Integer();
-						string name = nameReader.String();
-						//This is not entirely correct as the name may have changed, but whatever
-						UpdateSummoner(new SummonerDescription(name, id, accountId), false);
-						return true;
-					}
-					else
-					{
-						//We might be dealing with a new summoner
-						PublicSummoner publicSummoner = RPC.GetSummonerByName(summonerName);
-						if (publicSummoner == null)
+						if (nameReader.Read())
 						{
-							WriteLine("No such summoner: {0}", summonerName);
-							return false;
+							//The summoner already exists in the database
+							int id = nameReader.Integer();
+							accountId = nameReader.Integer();
+							string name = nameReader.String();
+							//This is not entirely correct as the name may have changed, but whatever
+							UpdateSummoner(new SummonerDescription(name, id, accountId), false);
+							return WorkerResult.Success;
 						}
-
-						using (var check = Command("select id from summoner where account_id = :account_id and region = :region"))
+						else
 						{
-							check.Set("account_id", publicSummoner.acctId);
-							check.Set("region", Profile.Identifier);
-							using (var checkReader = check.ExecuteReader())
+							//We might be dealing with a new summoner
+							PublicSummoner publicSummoner = RPC.GetSummonerByName(summonerName);
+							if (publicSummoner == null)
 							{
-								if (checkReader.Read())
-								{
-									//We are dealing with an existing summoner even though the name lookup failed
-									int id = checkReader.Integer();
-									UpdateSummoner(new SummonerDescription(publicSummoner.name, id, publicSummoner.acctId), true);
-								}
-								else
-									InsertNewSummoner(publicSummoner.acctId, publicSummoner.summonerId, publicSummoner.name, publicSummoner.internalName, publicSummoner.summonerLevel, publicSummoner.profileIconId);
-
+								WriteLine("No such summoner: {0}", summonerName);
+								return WorkerResult.NotFound;
 							}
-							return true;
+
+							using (var check = Command("select id from summoner where account_id = :account_id and region = :region"))
+							{
+								check.Set("account_id", publicSummoner.acctId);
+								check.Set("region", Profile.Identifier);
+								using (var checkReader = check.ExecuteReader())
+								{
+									if (checkReader.Read())
+									{
+										//We are dealing with an existing summoner even though the name lookup failed
+										int id = checkReader.Integer();
+										UpdateSummoner(new SummonerDescription(publicSummoner.name, id, publicSummoner.acctId), true);
+									}
+									else
+										InsertNewSummoner(publicSummoner.acctId, publicSummoner.summonerId, publicSummoner.name, publicSummoner.internalName, publicSummoner.summonerLevel, publicSummoner.profileIconId);
+
+								}
+								return WorkerResult.Success;
+							}
 						}
 					}
 				}
+			}
+			catch (RPCTimeoutException)
+			{
+				return WorkerResult.Timeout;
 			}
 		}
 	}
