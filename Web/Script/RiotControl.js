8c53bbca281d58f013da86869213701d66d50d1d
void <git.email.address@googlemail.com>
2012-03-28 15:12:01 +0200

    Implemented the summoner overview

--- Web/Script/RiotControl.js
+++ Web/Script/RiotControl.js
@@ -27,13 +27,48 @@ function apiCall(name, callArguments, callback)
                 throw 'API error in ' + path + ': ' + request.responseText;
         }
     }
-    request.open('GET', path);
+    request.open('GET', path, true);
     request.send();
 }
 
 function getById(id)
 {
     return document.getElementById(id);
+}
+
+function getResultString(response)
+{
+    var result = response.Result;
+    var messages = [];
+    messages['Success'] = 'Success.';
+    messages['NotFound'] = 'Unable to find summoner.';
+    messages['Timeout'] = 'The request has timed out.';
+    messages['NotConnected'] = 'The worker for this region is not connected.';
+    if(result in messages)
+        return messages[result];
+    else
+        return 'Unknown server response (' + result + ').';
+}
+
+function isSuccess(response)
+{
+    return response.Result == 'Success';
+}
+
+function padWithZeroes(number, zeroes)
+{
+    if(zeroes === undefined)
+        zeroes = 2;
+    var output = number.toString();
+    while(output.length < zeroes)
+        output = '0' + output;
+    return output;
+}
+
+function getTimestampString(timestamp)
+{
+    var date = new Date(timestamp * 1000);
+    return date.getFullYear() + '-' + padWithZeroes(date.getMonth()) + '-' + padWithZeroes(date.getDay()) + ' ' + padWithZeroes(date.getHours()) + ':' + padWithZeroes(date.getMinutes()) + ':' + padWithZeroes(date.getSeconds());
 }
 
 //URL functions
@@ -46,7 +81,7 @@ function getURL(path)
 function getBaseURL()
 {
     var mainScript = getById('mainScript');
-    if(mainScript == undefined)
+    if(mainScript === undefined)
         throw 'Unable to find the main script ID';
     var separator = '/';
     var tokens = mainScript.src.split(separator);
@@ -88,7 +123,7 @@ function initialise(regions, privileged)
     initialiseSystem(regions, privileged);
     installStringExtensions();
     loadStylesheet();
-    render(getIndex());
+    showIndex();
 }
 
 function installStringExtensions()
@@ -111,7 +146,19 @@ function addChild(input)
 {
     if(typeof input == 'string')
         input = text(input);
-    this.appendChild(input);
+    else if(typeof input == 'number')
+        input = text(input.toString());
+    try
+    {
+        this.appendChild(input);
+    }
+    catch(exception)
+    {
+        //Firebug only
+        if(console !== undefined)
+            console.trace();
+        throw exception;
+    }
 }
 
 function removeChildren()
@@ -197,6 +244,58 @@ function span()
     return createElement('span');
 }
 
+function bold()
+{
+    return createElement('b');
+}
+
+function anchor()
+{
+    return createElement('a');
+}
+
+function table()
+{
+    return createElement('table');
+}
+
+function tableRow()
+{
+    return createElement('tr');
+}
+
+function tableCell()
+{
+    return createElement('td');
+}
+
+function tableHead()
+{
+    return createElement('th');
+}
+
+//Riot Control JSON API
+
+function apiFindSummoner(region, summonerName, callback)
+{
+    apiCall('Search', [region, summonerName], function (response) { callback(region, response); } );
+}
+
+function apiUpdateSummoner(region, accountId, callback)
+{
+    apiCall('Update', [region, accountId], function (response) { callback(region, response); });
+}
+
+function apiGetSummonerProfile(region, accountId, callback)
+{
+    apiCall('Profile', [region, accountId], function (response) { callback(region, response); });
+}
+
+function apiGetMatchHistoryu(region, accountId, callback)
+{
+    apiCall('Games', [region, accountId], function (response) { callback(region, response); });
+}
+
 //Rendering/DOM functions
 
 function render(node)
@@ -237,14 +336,17 @@ function getRegionSelection()
     return selectNode;
 }
 
-function getIndex()
+function showIndex(errorResponse)
 {
     var container = diverse();
     container.id = 'indexForm';
 
     var description = paragraph();
     description.id = 'searchDescription';
-    description.add('Enter the name of the summoner you want to look up:');
+    if(errorResponse === undefined)
+        description.add('Enter the name of the summoner you want to look up:');
+    else
+        description.add(getErrorSpan(errorResponse.Result));
     var summonerNameField = textBox('summonerName');
     var regionSelection = getRegionSelection();
     var button = submitButton('Search', performSearch);
@@ -255,7 +357,7 @@ function getIndex()
     container.add(regionSelection);
     container.add(button);
 
-    return container;
+    render(container);
 }
 
 function setSearchFormState(state)
@@ -266,11 +368,16 @@ function setSearchFormState(state)
     button.disabled = !state;
 }
 
-function searchError(message)
+function getErrorSpan(message)
 {
     var node = span();
     node.className = 'error';
     node.add(message);
+}
+
+function searchError(message)
+{
+    var node = getErrorSpan(message);
     setSearchDescription(node);
     setSearchFormState(true);
 }
@@ -282,7 +389,118 @@ function setSearchDescription(node)
     paragraph.add(node);
 }
 
-//Event handlers
+function viewSummonerProfile(region, accountId)
+{
+    apiGetSummonerProfile(region, accountId, onSummonerProfileRetrieval);
+}
+
+function renderSummonerProfile(profile)
+{
+    var summoner = profile.Summoner;
+    var ratings = profile.Ratings;
+    var rankedStatistics = profile.RankedStatistics;
+    var unrankedStatistics = profile.UnrankedStatistics;
+    var dominionStatistics = profile.DominionStatistics;
+
+    var region = system.regions[summoner.Region].abbreviation;
+
+    var profileIcon = image('Profile/profileIcon' + summoner.ProfileIcon + '.jpg', summoner.SummonerName + "'s profile icon");
+    profileIcon.id = 'profileIcon';
+
+    var gamesPlayed = 0;
+    for(i in ratings)
+    {
+        var statistics = ratings[i];
+        gamesPlayed += statistics.Wins;
+        gamesPlayed += statistics.Losses;
+    }
+
+    var overviewFields1 =
+    [
+        ['Summoner name', summoner.SummonerName],
+        ['Internal name', summoner.InternalName],
+        ['Region', region],
+        ['Summoner level', summoner.SummonerLevel],
+        ['Non-custom games played', gamesPlayed],
+        ['Account ID', summoner.AccountId],
+        ['Summoner ID', summoner.SummonerId],
+    ];
+
+    var updateNowLink = anchor();
+    updateNowLink.onclick = function()
+    {
+        updateSummoner(region, summoner.AccountId);
+    };
+    updateNowLink.add('Update now');
+
+    var automaticUpdatesLink = anchor();
+    automaticUpdatesLink.onclick = function()
+    {
+        enableAutomaticUpdates(region, summoner.AccountId);
+    };
+    automaticUpdatesLink.add('enable');
+
+    var automaticUpdatesDescription = span();
+    automaticUpdatesDescription.id = 'automaticUpdates';
+    automaticUpdatesDescription.add('No (');
+    automaticUpdatesDescription.add(automaticUpdatesLink);
+    automaticUpdatesDescription.add(')');
+
+    var matchHistory = anchor();
+    matchHistory.onclick = function()
+    {
+        viewMatchHistory(region, summoner.AccountId);
+    };
+    matchHistory.add('View games');
+
+    var overviewFields2 =
+    [
+        ['Match history', matchHistory],
+        ['Manual update', updateNowLink],
+        ['Is updated automatically', summoner.UpdateAutomatically ? 'Yes' : automaticUpdatesDescription],
+        ['First update', getTimestampString(summoner.TimeCreated)],
+        ['Last update', getTimestampString(summoner.TimeUpdated)],
+    ];
+
+    var container = diverse();
+    container.id = 'summonerHeader';
+    container.add(profileIcon);
+    container.add(getOverviewTable(overviewFields1));
+    container.add(getOverviewTable(overviewFields2));
+
+    render(container);
+}
+
+function getOverviewTable(fields)
+{
+    var output = table();
+    output.className = 'summonerOverview';
+    for(var i in fields)
+    {
+        var entry = fields[i];
+        var description = entry[0];
+        var value = entry[1];
+
+        var row = tableRow();
+
+        var descriptionCell = tableCell();
+        var boldDescription = bold();
+        boldDescription.add(description);
+        descriptionCell.add(boldDescription);
+
+        var valueCell = tableCell();
+        valueCell.add(value);
+
+        row.add(descriptionCell);
+        row.add(valueCell);
+
+        output.add(row);
+    }
+
+    return output;
+}
+
+//Button/link handlers
 
 function performSearch()
 {
@@ -292,20 +510,54 @@ function performSearch()
     var regionSelect = getById('region');
     var region = regionSelect.options[regionSelect.selectedIndex].value;
     setSearchDescription('Searching for "' + summonerName + '"...');
-    apiCall('Search', [region, summonerName], processSearchResult);
+    apiFindSummoner(region, summonerName, onSearchResult);
 }
 
-function processSearchResult(response)
+function viewMatchHistory(region, accountId)
 {
-    var result = response.Result;
-    if(result == 'Success')
+}
+
+function updateSummoner(region, accountId)
+{
+}
+
+function enableAutomaticUpdates(region, accountId)
+{
+}
+
+//JSON request handlers
+
+function onSearchResult(region, response)
+{
+    if(isSuccess(response))
+        viewSummonerProfile(region, response.AccountId);
+    else
+        searchError(response);
+}
+
+function onSummonerProfileRetrieval(region, response)
+{
+    if(isSuccess(response))
     {
-        alert('Success: ' + response.AccountId);
+        var profile = response.Profile;
+        var summoner = profile.Summoner;
+        if(!summoner.HasBeenUpdated)
+        {
+            //This means that this summoner entry in the database was only created by a search for a summoner name.
+            //It does not actually hold any useful information yet and needs to be updated first.
+            apiUpdateSummoner(region, summoner.AccountId, function (region, response) { onSummonerUpdate(region, summoner.AccountId, response); } );
+            return;
+        }
+        renderSummonerProfile(profile);
     }
-    else if(result == 'NotFound')
-        searchError('Unable to find summoner.');
-    else if(result == 'Timeout')
-        searchError('The request has timed out.');
-    else if(result == 'NotConnected')
-        searchError('The worker for this region is not connected.');
+    else
+        showIndex(response);
+}
+
+function onSummonerUpdate(region, accountId, response)
+{
+    if(isSuccess(response))
+        renderSummonerProfile(response.Profile);
+    else
+        showIndex(response);
 }
