adf5ec7eca178a4c63ddc9b53b1750a5f3620709
void <git.email.address@googlemail.com>
2012-04-01 16:22:33 +0200

    Switched to the new revision system

---
+++ RiotControl/Properties/AssemblyInfo.template.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RiotControl")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("9e9ac50c-a570-4a9f-90f2-fdf220c5464b")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
