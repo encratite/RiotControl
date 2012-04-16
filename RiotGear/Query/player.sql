5e48edf3a4d1b99ea482e7468fa9c39a77844673
void <git.email.address@googlemail.com>
2012-04-16 03:34:28 +0200

    Implemented a database upgrade for the player.game_id null issue

---
+++ RiotGear/Query/player.sql
@@ -0,0 +1,81 @@
+create table player
+(
+        game_id integer not null,
+        team_id integer not null,
+        summoner_id integer not null,
+
+        ping integer not null,
+        time_spent_in_queue integer not null,
+
+        premade_size integer not null,
+
+        experience_earned integer not null,
+        boosted_experience_earned integer not null,
+
+        ip_earned integer not null,
+        boosted_ip_earned integer not null,
+
+        summoner_level integer not null,
+
+        summoner_spell1 integer not null,
+        summoner_spell2 integer not null,
+
+        champion_id integer not null,
+
+        skin_name text,
+        skin_index integer not null,
+
+        champion_level integer not null,
+
+        items integer array[6] not null,
+
+        kills integer not null,
+        deaths integer not null,
+        assists integer not null,
+
+        minion_kills integer not null,
+
+        gold integer not null,
+
+        damage_dealt integer not null,
+        physical_damage_dealt integer not null,
+        magical_damage_dealt integer not null,
+
+        damage_taken integer not null,
+        physical_damage_taken integer not null,
+        magical_damage_taken integer not null,
+
+        total_healing_done integer not null,
+
+        time_spent_dead integer not null,
+
+        largest_multikill integer not null,
+        largest_killing_spree integer not null,
+        largest_critical_strike integer not null,
+
+        --Summoner's Rift/Twisted Treeline
+
+        neutral_minions_killed integer,
+
+        turrets_destroyed integer,
+        inhibitors_destroyed integer,
+
+        --Dominion
+
+        nodes_neutralised integer,
+        node_neutralisation_assists integer,
+        nodes_captured integer,
+
+        victory_points integer,
+        objectives integer,
+
+        total_score integer,
+        objective_score integer,
+        combat_score integer,
+
+        rank integer,
+
+        foreign key(game_id) references game(id),
+        foreign key(team_id) references team(id),
+        foreign key(summoner_id) references summoner(id)
+);
--- RiotGear/StatisticsService/UpgradeDatabase.cs
+++ RiotGear/StatisticsService/UpgradeDatabase.cs
@@ -1,9 +1,112 @@
 ﻿using System;
+using System.IO;
+using System.Data.Common;
 
 namespace RiotGear
 {
 	public partial class StatisticsService
 	{
+
+		void UnknownPlayerUpgrade(DbConnection connection)
+		{
+			//This is required for the r248 fix in the unknown_player table
+			//The field "account_id" had to be renamed to "summoner_id" in that version
+			//First check if this database is actually affected by that bug
+			bool isAffected = false;
+			using (var pragma = new DatabaseCommand("pragma table_info(unknown_player)", connection))
+			{
+				using (var reader = pragma.ExecuteReader())
+				{
+					while (reader.Read())
+					{
+						string name = (string)reader.Get("name");
+						if (name == "account_id")
+						{
+							isAffected = true;
+							break;
+						}
+					}
+				}
+			}
+			if (isAffected)
+			{
+				const string createTableQuery = "create table unknown_player(team_id integer not null, champion_id integer not null, summoner_id integer not null, foreign key (team_id) references team(id))";
+
+				WriteLine("This database is affected by the pre-r248 unknown_player bug. Attempting to upgrade it.");
+				using (var transaction = connection.BeginTransaction())
+				{
+					//Rename the old table to a temporary name
+					using (var renameTable = new DatabaseCommand("alter table unknown_player rename to broken_unknown_player", connection))
+						renameTable.Execute();
+					//Create the new table
+					using (var createTable = new DatabaseCommand(createTableQuery, connection))
+						createTable.Execute();
+					//Insert the data from the old table into the new table
+					using (var insert = new DatabaseCommand("insert into unknown_player (team_id, champion_id, summoner_id) select team_id, champion_id, account_id from broken_unknown_player", connection))
+						insert.Execute();
+					//Remove the old table
+					using (var dropTable = new DatabaseCommand("drop table broken_unknown_player", connection))
+						dropTable.Execute();
+					//Commit the transaction
+					transaction.Commit();
+					//Vacuum
+					using (var vacuum = new DatabaseCommand("vacuum", connection))
+						vacuum.Execute();
+				}
+				WriteLine("Upgrade succeeded.");
+			}
+		}
+
+		void GameIdUpgrade(DbConnection connection)
+		{
+			//player.game_id was nullable prior to r348, should have been not null all along
+			bool isAffected = false;
+			using (var pragma = new DatabaseCommand("pragma table_info(player)", connection))
+			{
+				using (var reader = pragma.ExecuteReader())
+				{
+					while (reader.Read())
+					{
+						object notNullObject = reader.Get("notnull");
+						bool isNotNull = (int)(long)notNullObject == 1;
+						if (!isNotNull)
+						{
+							isAffected = true;
+							break;
+						}
+					}
+				}
+			}
+
+			if (isAffected)
+			{
+				WriteLine("This database is affected by the pre-348 player.game_id bug. Attempting to upgrade it");
+
+				using (var transaction = connection.BeginTransaction())
+				{
+					//Rename the old table to a temporary name
+					using (var renameTable = new DatabaseCommand("alter table player rename to broken_player", connection))
+						renameTable.Execute();
+					//Create the new table
+					using (var createTable = new DatabaseCommand(Properties.Resources.CreateTablePlayer, connection))
+						createTable.Execute();
+					//Insert the data from the old table into the new table
+					string fieldString = Properties.Resources.PlayerFields.Replace("\n", " ");
+					using (var insert = new DatabaseCommand(string.Format("insert into player ({0}) select {0} from broken_player", fieldString), connection))
+						insert.Execute();
+					//Remove the old table
+					using (var dropTable = new DatabaseCommand("drop table broken_player", connection))
+						dropTable.Execute();
+					//Commit the transaction
+					transaction.Commit();
+					//Vacuum
+					using (var vacuum = new DatabaseCommand("vacuum", connection))
+						vacuum.Execute();
+				}
+				WriteLine("Upgrade succeeded.");
+			}
+		}
+
 		//This function is required to upgrade old database formats to newer ones
 		void UpgradeDatabase()
 		{
@@ -12,52 +115,8 @@ namespace RiotGear
 				//This legacy upgrade is only required for old SQLite databases
 				if (Provider.IsSQLite())
 				{
-					//This is required for the r248 fix in the unknown_player table
-					//The field "account_id" had to be renamed to "summoner_id" in that version
-					//First check if this database is actually affected by that bug
-					bool isAffected = false;
-					using (var pragma = new DatabaseCommand("pragma table_info(unknown_player)", connection))
-					{
-						using (var reader = pragma.ExecuteReader())
-						{
-							while (reader.Read())
-							{
-								string name = (string)reader.Get("name");
-								if (name == "account_id")
-								{
-									isAffected = true;
-									break;
-								}
-							}
-						}
-					}
-					if (isAffected)
-					{
-						const string createTableQuery = "create table unknown_player(team_id integer not null, champion_id integer not null, summoner_id integer not null, foreign key (team_id) references team(id))";
-
-						WriteLine("This database is affected by the pre-r248 unknown_player bug. Attempting to upgrade it.");
-						using (var transaction = connection.BeginTransaction())
-						{
-							//Rename the old table to a temporary name
-							using (var renameTable = new DatabaseCommand("alter table unknown_player rename to broken_unknown_player", connection))
-								renameTable.Execute();
-							//Create the new table
-							using (var createTable = new DatabaseCommand(createTableQuery, connection))
-								createTable.Execute();
-							//Insert the data from the old table into the new table
-							using (var insert = new DatabaseCommand("insert into unknown_player (team_id, champion_id, summoner_id) select team_id, champion_id, account_id from broken_unknown_player", connection))
-								insert.Execute();
-							//Remove the old table
-							using (var dropTable = new DatabaseCommand("drop table broken_unknown_player", connection))
-								dropTable.Execute();
-							//Commit the transaction
-							transaction.Commit();
-							//Vacuum
-							using (var vacuum = new DatabaseCommand("vacuum", connection))
-								vacuum.Execute();
-						}
-						WriteLine("Upgrade succeeded.");
-					}
+					UnknownPlayerUpgrade(connection);
+					GameIdUpgrade(connection);
 				}
 			}
 		}
